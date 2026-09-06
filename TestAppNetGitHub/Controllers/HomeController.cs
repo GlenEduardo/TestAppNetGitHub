19d190aa7cb4bfd623de8e473d6e40b81ebd9183
eduardo <glenreinoso@outlook.com>
2026-09-06 14:09:07 -0500

    Feature Contacto creado correctamente

--- TestAppNetGitHub/Controllers/HomeController.cs
+++ TestAppNetGitHub/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace TestAppNetGitHub.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
     }
 }
